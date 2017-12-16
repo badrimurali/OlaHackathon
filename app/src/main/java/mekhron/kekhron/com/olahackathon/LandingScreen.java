15869cd0168e46e49ff08d003f5bf3c0e3d6d8b7
badrimurali <badriitindia@gmail.com>
2017-12-17 03:44:14 +0530

    recycler songs view

--- app/src/main/java/mekhron/kekhron/com/olahackathon/LandingScreen.java
+++ app/src/main/java/mekhron/kekhron/com/olahackathon/LandingScreen.java
@@ -3,6 +3,8 @@ package mekhron.kekhron.com.olahackathon;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.google.gson.Gson;
 
@@ -12,15 +14,16 @@ import mekhron.kekhron.com.olahackathon.Model.Song;
 import mekhron.kekhron.com.olahackathon.Rest.RestServices;
 import mekhron.kekhron.com.olahackathon.Utils.SharedPref;
 
-public class MainActivity extends AppCompatActivity {
+public class LandingScreen extends AppCompatActivity {
 
     private ProgressDialog progressDialog;
+    private RecyclerView rvSongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        progressDialog = new ProgressDialog(MainActivity.this);
+        setContentView(R.layout.activity_landing_screen);
+        progressDialog = new ProgressDialog(LandingScreen.this);
         progressDialog.setMessage("loading");
         progressDialog.setCancelable(false);
         progressDialog.show();
@@ -30,15 +33,26 @@ public class MainActivity extends AppCompatActivity {
                 if (progressDialog != null && progressDialog.isShowing())
                     progressDialog.dismiss();
                 System.out.println("asdf songs "+ new Gson().toJson(songs));
-                SharedPref.saveSongs(MainActivity.this, songs);
+                SharedPref.saveSongs(LandingScreen.this, songs);
+                initViews(songs);
             }
         }, new Consumer<String>() {
             @Override
             public void consume(String s) {
                 if (progressDialog != null && progressDialog.isShowing())
                     progressDialog.dismiss();
-                CustomSnackBar.show(MainActivity.this, "Songs fetch Failed!!", "OK");
+                CustomSnackBar.show(LandingScreen.this, "Songs fetch Failed!!", "OK");
             }
         });
+    }
+
+    private void initViews(List<Song> songs) {
+        rvSongs = findViewById(R.id.rv_songs);
+        rvSongs.setHasFixedSize(false);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(LandingScreen.this);
+        rvSongs.setNestedScrollingEnabled(false);
+        rvSongs.setLayoutManager(layoutManager);
+        SongsAdapter adapter = new SongsAdapter(LandingScreen.this, songs);
+        rvSongs.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/mekhron/kekhron/com/olahackathon/SongsAdapter.java
@@ -0,0 +1,69 @@
+package mekhron.kekhron.com.olahackathon;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.List;
+
+import mekhron.kekhron.com.olahackathon.Model.Song;
+
+/**
+ * Created by badri on 17/12/17.
+ */
+
+public class SongsAdapter extends RecyclerView.Adapter<SongsAdapter.ViewHolder> {
+    private Context context;
+    private List<Song> songs;
+
+    public SongsAdapter(Context context, List<Song> songs) {
+        this.context = context;
+        this.songs = songs;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        return new ViewHolder(inflater.inflate(R.layout.item_songs, null));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.setViews(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.songs.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvSongName, tvArtistsName;
+        private ImageView ivSong;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvSongName = itemView.findViewById(R.id.tv_song_name);
+            tvArtistsName = itemView.findViewById(R.id.tv_artists_name);
+            ivSong = itemView.findViewById(R.id.iv_song_image);
+        }
+
+        public void setViews(int position) {
+            Song song = songs.get(position);
+            tvSongName.setText(song.getSong());
+            tvArtistsName.setText(song.getArtists());
+            Glide.with(context)
+                    .load(Uri.parse(song.getCover_image()))
+                    .apply(new RequestOptions().centerCrop())
+                    .into(ivSong);
+        }
+    }
+}
